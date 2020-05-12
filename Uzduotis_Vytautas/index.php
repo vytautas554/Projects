04db20ca19b4441d32a1d6873fd2ae1282f10096
vytautaskrapikas@gmail.com <vytautaskrapikas@gmail.com>
2020-05-12 14:28:48 +0300

    Klaidos taisymas

--- Uzduotis_Vytautas/index.php
+++ Uzduotis_Vytautas/index.php
@@ -6,7 +6,7 @@ $visosCars = getCars();
 $car = mysqli_fetch_assoc($visosCars);
 
 while ($car == true) {
-    echo "<a href='template-doctor.php?aa=$car[id]'> $car[number]
+    echo "<a href='template-cars.php?aa=$car[id]'> $car[number]
     $car[year_made] $car[model] </a><br>";
     $car = mysqli_fetch_assoc($visosCars);
 }
