5199ebeefa30d67795cc1ae7851b6cd3e117eb22
vytautaskrapikas@gmail.com <vytautaskrapikas@gmail.com>
2020-12-18 10:53:48 +0200

    Contact data

---
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import Button from '../../../components/UI/Button/Button';
+import styles from './ContactData.module.css';
+
+class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: ''
+        }
+    }
+
+    render () {
+        return (
+            <div className={styles.ContactData}>
+                <h4>Enter your contact data</h4>
+                <form>
+                    <input className={styles.Input} type="text" name="name" placeholder="Your name"/>
+                    <input className={styles.Input} type="email" name="email" placeholder="Your mail"/>
+                    <input className={styles.Input} type="text" name="street" placeholder="Your street"/>
+                    <input className={styles.Input} type="text" name="postal" placeholder="Your postal Code"/>
+                    <Button btnType="Success">ORDER</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ContactData;
